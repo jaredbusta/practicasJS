c92517f06fc475cc83a0358891495f1304f3b24d
Jared Bustamante <jaredbusta@gmail.com>
2021-07-15 22:41:55 -0700

    funciones para calcular area y perimetro de figuras y formulario para calcular perimetro de cuadrado

--- js/figuras.js
+++ js/figuras.js
@@ -1,25 +1,39 @@
+const array=[1,2,3,4,5,6,7];
+// const mitad = Number.half(array.length);
+// const mitad = Number(array.length)/2; // 3.5
+const mitad =  array.length/2;//NaN
+// const mitad = array[50%];//syntaxis error
+
+
+console.log(mitad);
+
 const PI = Math.PI;
 
 function perimetroCuadrado(lado){
     console.log("El permíetro del cuadrado es: " + lado*4 +" cm");
+    return lado * 4;
 }
 function areaCuadrado(lado){
     console.log("El áera del cuadrado es:" + (Math.pow(lado,2) ) + "cm cuadrados");
+    return Math.pow(lado,2);
 }
 function perimetroTriangulo(lado1,lado2,base){
     console.log("El perímetro del triángulo es: "+ (lado1 + lado2 + base) + " cm" );
+    return (lado1 + lado2 + base);
 }
 
 function areaTriangulo(base,altura){
     console.log("El área del triángulo es: " + ((base * altura)/2)+ " cm cuadrados");
+    return ((base*altura)/2);
 }
 
 function perimetro_circulo(radio){
     console.log("La circunferencia del círculo es: " + ((2 * PI) * radio)+ " cm");
-
+    return ((2 * PI) * radio);
 }
 function area_circulo(radio){
     console.log("El área del círculo es: "+ (PI * ( Math.pow(radio,2) )) +" cm cuadrados");
+    return (PI * ( Math.pow(radio,2) ));
 }
 
 
@@ -46,3 +60,15 @@ console.group("Círculo");
     perimetro_circulo(radio);
     area_circulo(radio);
 console.groupEnd();
+
+
+// listener de botones
+function cuadradoCalcularPerimetro(){
+    const lado = document.getElementById("input_cuadrado").value;
+    const perimetro = perimetroCuadrado(lado);
+    alert(perimetro);
+    
+}
+function cuadradoCalcularArea(){
+
+}
