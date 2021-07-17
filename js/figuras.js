06229cc0a73c48e8978657b29b4d18e2989a8011
Jared Bustamante <jaredbusta@gmail.com>
2021-07-17 09:04:21 -0700

    Calculo de altura de triangulo isósceles

--- js/figuras.js
+++ js/figuras.js
@@ -37,6 +37,22 @@ function area_circulo(radio){
 }
 
 
+function altura_triangulo_isosceles( lado1, lado2, lado3){
+    let altura =0;
+    let lados = [ lado1, lado2, lado3]; // valores desordenados
+    lados.sort(); // ordenacion ascendente pos 0 y 1 son lados, posicion 2 es la base
+    // valida que ningun lado sea 0 y que solo un lado sea diferente
+    if(!lados.includes(0) && lados.some( (val,i)=> lados.indexOf(val) != i ) && lados[0]!=lados[2]   ){ // existe un elemento repetido
+        let repetido =  lados.filter((l,i)=>lados.indexOf(l)!==i  ); // filtra los repetidos
+        // identifica los lados y la base
+        let lado = repetido[0];
+        let base  = lados[0] !== lado ? lados[0] : lados[2]; // operador ternario para definir la base del triangulo
+        altura = Math.sqrt(  Math.pow(lado,2) -  Math.pow( ( base / 2  ), 2   ) ) ; // operacion de altura
+    }
+    return altura;
+}
+
+
 
 //===================================================
 let lado_cuadado =5;
@@ -62,7 +78,6 @@ console.group("Círculo");
 console.groupEnd();
 
 
-// listener de botones
 function cuadradoCalcularPerimetro(){
     const lado = document.getElementById("input_cuadrado").value;
     const perimetro = perimetroCuadrado(lado);
@@ -70,5 +85,43 @@ function cuadradoCalcularPerimetro(){
     
 }
 function cuadradoCalcularArea(){
+    const lado = document.getElementById("input_cuadrado").value;
+    const perimetro = areaCuadrado(lado);
+    alert(perimetro);
+}
+function trianguloPerimetro(){
+    const lado1 = document.getElementById("input_lado_1_trinagulo").value;
+    const lado2 = document.getElementById("input_lado_2_trinagulo").value;
+    const lado3 = document.getElementById("input_lado_3_trinagulo").value;
+    const perimetro = perimetroTriangulo( parseFloat(lado1) , parseFloat(lado2),parseFloat(lado3));
+    alert(perimetro);
+}
+function trianguloArea(){
+    const base = document.getElementById("input_base_trinagulo").value;
+    const alto = document.getElementById("input_alto_trinagulo").value;
+    const area = areaTriangulo( parseFloat(base) , parseFloat(alto));
+    alert(area);
+}
+function calculaPerimetroCirculo(){
+    const radio = document.getElementById("input_radio_circulo").value;
+    const perimetro = perimetro_circulo(parseFloat(radio));
+    alert(perimetro);
+}
+function calculaAreaCirculo(){
+    const radio = document.getElementById("input_radio_circulo").value;
+    const area = area_circulo(parseFloat(radio));
+    alert(area);
+}
 
+function calcularAlturaTrianguloIsosceles(){
+    const lado1= document.getElementById("input_lado_1_trinagulo_isosceles").value;
+    const lado2= document.getElementById("input_lado_2_trinagulo_isosceles").value;
+    const lado3= document.getElementById("input_lado_3_trinagulo_isosceles").value;
+    const altura = altura_triangulo_isosceles(lado1,lado2,lado3);
+    if(altura>0){
+        alert("la altura es: " + altura);
+    }else{
+        alert("Los lados: ["+  lado1+", "+ lado2 +" y "+  lado3    + "] no cumplen con las características de un triángulo isósceles");
+    }
+    
 }
